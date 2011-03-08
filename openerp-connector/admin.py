0df523a5d9e2f02bc338c45b2c01efed3cefdbbd
gdelpierre <nameautomatic@gmail.com>
2011-03-08 18:40:44 +0100

    First commit 	new file:   openerp-connector/README 	new file:   openerp-connector/__init__.py 	new file:   openerp-connector/admin.py 	new file:   openerp-connector/models.py 	new file:   openerp-connector/settings.py 	new file:   openerp-connector/tests.py 	new file:   openerp-connector/utils.py 	new file:   openerp-connector/views.py

---
+++ openerp-connector/admin.py
@@ -0,0 +1,12 @@
+from openerp-connector.models import AccountingEntry
+from django.contrib import admin
+
+class ConnectorAdmin(admin.ModelAdmin):
+
+	fields = ['lastname', 'nickname', 'transaction_number', 'payment_datetime', 'nature', 'amount']
+	list_display = ('lastname', 'nickname', 'transaction_number', 'payment_datetime', 'nature', 'amount')
+	list_filter = ['payment_datetime', 'nature', 'lastname', 'nickname']
+	search_fields = ['transaction_number', 'lastname', 'nickname']
+	date_hierarchy = 'payment_datetime'
+
+admin.site.register(AccountingEntry, ConnectorAdmin)
